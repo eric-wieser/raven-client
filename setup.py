3b23e2ecf26e462e20ab5d5370e6e47e64fcca06
Eric Wieser <wieser.eric@gmail.com>
2014-10-18 13:25:19 +0100

    Update version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'ravenclient',
   py_modules = ['ravenclient'],
-  version = '0.2',
+  version = '0.3',
   description = 'A python requests adapter to automatically login to the Cambridge University Raven Login',
   author = 'Eric Wieser',
   author_email = 'wieser.eric@gmail.com',
-  url = 'https://github.com/eric-wieser/raven-client', # use the URL to the github repo
-  download_url = 'https://github.com/eric-wieser/raven-client/tarball/0.1', # I'll explain this in a second
-  keywords = ['authentication', 'requests', 'raven'], # arbitrary keywords
+  url = 'https://github.com/eric-wieser/raven-client',
+  download_url = 'https://github.com/eric-wieser/raven-client/tarball/0.3',
+  keywords = ['authentication', 'requests', 'raven'],
   classifiers = [],
 )
