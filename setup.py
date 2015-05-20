1e130fc0b17b9e5f09644bf67a1d74d4ff5ab8fc
Eric Wieser <wieser.eric@gmail.com>
2015-05-20 23:43:06 +0100

    Fix under-specified versions

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'ravenclient',
   py_modules = ['ravenclient'],
-  install_requires = ['beautifulsoup4 >= 4.3'],
-  version = '0.4',
+  install_requires = ['beautifulsoup4 >= 4.3.0', 'requests >= 2.7.0'],
+  version = '0.6',
   description = 'A python requests adapter to automatically login to the Cambridge University Raven Login',
   author = 'Eric Wieser',
   author_email = 'wieser.eric+raven@gmail.com',
   url = 'https://github.com/eric-wieser/raven-client',
-  download_url = 'https://github.com/eric-wieser/raven-client/tarball/0.4',
+  download_url = 'https://github.com/eric-wieser/raven-client/tarball/0.6',
   keywords = ['authentication', 'requests', 'raven'],
   classifiers = [],
 )
