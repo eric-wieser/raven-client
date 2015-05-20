c31a5be6f135f1fd0c9f95e1ac96f1e25817ed0d
Eric Wieser <wieser.eric@gmail.com>
2015-05-20 23:33:16 +0100

    Fix install_requires

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'ravenclient',
   py_modules = ['ravenclient'],
-  version = '0.3',
+  install_requires = ['beautifulsoup4 >= 4.3'],
+  version = '0.4',
   description = 'A python requests adapter to automatically login to the Cambridge University Raven Login',
   author = 'Eric Wieser',
-  author_email = 'wieser.eric@gmail.com',
+  author_email = 'wieser.eric+raven@gmail.com',
   url = 'https://github.com/eric-wieser/raven-client',
-  download_url = 'https://github.com/eric-wieser/raven-client/tarball/0.3',
+  download_url = 'https://github.com/eric-wieser/raven-client/tarball/0.4',
   keywords = ['authentication', 'requests', 'raven'],
   classifiers = [],
 )
