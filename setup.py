2ff17bc77797905c98924af6f056de723bf4f5da
Eric Wieser <wieser.eric@gmail.com>
2017-02-16 17:57:53 +0000

    Version 0.7.0

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
+
+version = '0.7.0'
+
 setup(
   name = 'ravenclient',
   py_modules = ['ravenclient'],
   install_requires = ['beautifulsoup4 >= 4.3.0', 'requests >= 2.7.0'],
-  version = '0.6',
+  version = version,
   description = 'A python requests adapter to automatically login to the Cambridge University Raven Login',
   author = 'Eric Wieser',
   author_email = 'wieser.eric+raven@gmail.com',
   url = 'https://github.com/eric-wieser/raven-client',
-  download_url = 'https://github.com/eric-wieser/raven-client/tarball/0.6',
+  download_url = 'https://github.com/eric-wieser/raven-client/tarball/'+version,
   keywords = ['authentication', 'requests', 'raven'],
   classifiers = [
     'License :: OSI Approved :: MIT License',
